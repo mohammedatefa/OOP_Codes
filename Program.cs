97d24e902c810c2894b98b7568776e79cbb586e6
M.Atef <142452016+mohammedatefa@users.noreply.github.com>
2023-09-25 20:36:37 +0300

    oop_operators
    apply operators in oop 
    in this code use the property and  constructor chained 
    - apply compare operators [<,>,=<.=>]
    - apply equal operator == && != 
    - apply + & - operators
    - incriment & decremint ++ &--

--- Program.cs
+++ Program.cs
@@ -1,76 +1,16 @@
-﻿using System.Reflection.Metadata;
-
-namespace Day_fourandFive_oop
+﻿namespace Day_six_oop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, polymorphism!");
-        }
-    }
-    public class shape
-    {
-        public int x;
-        public int y;
+            ComplexNumber a = new ComplexNumber(2, 3);
+            ComplexNumber b = new ComplexNumber(4, 1);
 
-        public shape(int x,int y)
-        {
-            this.x=x;
-            this.y=y;  
-        }
-
-        public virtual void  display()
-        {
-            Console.WriteLine($"the x is{x} and y is {y}");
-        }
-    }
-    public class circle:shape
-    {
-        public int radiuse;
-        public circle(int radiuse) : base(1, 2)
-        {
-
-            this.radiuse = radiuse; 
-
-        }
-        public void area()
-        {
-            double area = (22 / 7) * Math.Sqrt(radiuse);
-            Console.WriteLine(area);
-        }
-        public void area(int radius)
-        {
-            double area = (22 / 7) * Math.Sqrt(radiuse);
-            Console.WriteLine(area);
-        }
-
-        public override void display()
-        {
-            base.x += 1;
-            base.y += 2;
-            Console.WriteLine($"the x is{x} and y is {y}");
+            ComplexNumber increment = a;
+            Console.WriteLine($"Incremented a: {++increment}"); 
+            Console.WriteLine($"Incremented a: {increment++}");
 
         }
     }
-    public class rect : shape
-    {
-        public rect() : base(1, 2)
-        {
-
-        }
-    }
-
-    public class ellipse : circle
-    {
-        public ellipse() : base(10)
-        {
-
-        }
-        public new void display()
-        {
-            Console.WriteLine("we are in ellipse sahpe");
-        } 
-    }
-
 }
---
+++ complexNumber.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_six_oop
+{
+    public class ComplexNumber
+    {
+        private double real;
+        private double imaginary;
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public ComplexNumber(double real) : this(real, 0)
+        {
+        }
+
+        public double Real
+        {
+            get { return real; }
+            set { real = value; }
+        }
+
+        public double Imaginary
+        {
+            get { return imaginary; }
+            set { imaginary = value; }
+        }
+
+        public double GetMagnitude()
+        {
+            return Math.Sqrt(real * real + imaginary * imaginary);
+        }
+
+        public static bool operator >=(ComplexNumber a, ComplexNumber b)
+        {
+            return a.GetMagnitude() >= b.GetMagnitude();
+        }
+
+        public static bool operator <=(ComplexNumber a, ComplexNumber b)
+        {
+            return a.GetMagnitude() <= b.GetMagnitude();
+        }
+
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        }
+
+        public static ComplexNumber operator ++(ComplexNumber a)
+        {
+            return new ComplexNumber(a.Real + 1, a.Imaginary + 1);
+        }
+
+        public static implicit operator ComplexNumber(double value)
+        {
+            return new ComplexNumber(value);
+        }
+
+        public static explicit operator double(ComplexNumber complex)
+        {
+
+            return complex.Real;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (GetType() != obj.GetType())
+                return false;
+            else
+            {
+                ComplexNumber other = (ComplexNumber)obj;
+                return real == other.real && imaginary == other.imaginary;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{real} + {imaginary}i";
+        }
+    }
+}
