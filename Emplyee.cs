51f561de3304e10c0ca85019dd684b0621865116
M.Atef <142452016+mohammedatefa@users.noreply.github.com>
2023-09-25 20:17:03 +0300

    Add files via upload
    i explain the relationships between classes 
    1- association 
    2- aggregation
    3-composition 
    4-inhertence

---
+++ Emplyee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_three
+{
+    public class Person
+    {
+        public int Id;
+        public string Name;
+    }
+
+    public class Emloyee:Person
+    {
+        protected int _age;
+        protected string _certification;
+
+        public string title;
+        internal int Phone;
+        protected internal string Adress;
+
+       public void shoData()
+        {
+            Console.WriteLine($"the name is {Name} and its age is {_age} he is live in {Adress}");
+        }
+
+    }
+    public class manger:Emloyee
+    {
+        
+    } 
+}
