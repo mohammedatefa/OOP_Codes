f5d74f21f1d54893cae874afacd4a0d0cfc99ca8
M.Atef <142452016+mohammedatefa@users.noreply.github.com>
2023-09-25 19:46:23 +0300

    OOP-Constructros
    Create create first oop project and using constructors in it

---
+++ Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_one_oop
+{
+    internal class Employee
+    {
+        #region Fields
+        public int id;
+        public string name;
+        public int age;
+        public int salary;
+        private int experinceYear = 3;
+        #endregion
+
+        #region defualt_constructor
+        public Employee()
+        {
+            this.id = 1;
+            this.name = "mohamed";
+            this.age = 24;
+            this.salary = 7500;
+            this.experinceYear = 3;
+        }
+        #endregion
+
+        #region parametrized_constructor
+        public Employee(int id, string name, int age, int salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+
+        }
+        #endregion
+
+        #region copy_constructor
+        public Employee(string name, int age, int salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+
+        }
+        #endregion
+
+        #region Display Employee_Info
+        public void EmpInfo()
+        {
+            Console.WriteLine($"you are Employee in our company with \n ID :- {this.id} \nName :- {this.name} \n age :- {this.age} \n Experince Years :- {this.experinceYear}\n Salary :- {this.salary} ");
+        }
+        #endregion
+
+        #region bounce_expereince
+        public void bounce_expereince() {
+            double bounce_expereince;
+            bounce_expereince = this.salary * 0.1;
+            double salaryAfterBounce = this.salary + bounce_expereince;
+            Console.WriteLine($"salaryAfterBounce:- {salaryAfterBounce} with {this.experinceYear} years of experince");
+
+        }
+
+        #endregion
+
+        #region deepcopy_constructor
+        public Employee(Employee e)
+        {
+            this.id = e.id;
+            this.name = e.name;
+            this.salary = e.salary;
+            this.age = e.age;
+        }
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace day_one_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 6 employees instances 
+            Employee[] employees = new Employee[5];
+
+
+            #region first_employee
+            employees[0]=new Employee(1,"Ahmed",24,8500);
+            #endregion
+
+            #region second_employee using normal way
+            //second Employee with deep copy
+            //employees[1] = new Employee();
+
+            //employees[1].id = employees[0].id;
+            //employees[1].name = employees[0].name;
+            //employees[1].age = employees[0].age;
+            //employees[1].salary = employees[0].salary;
+
+            #endregion
+
+            #region second_employee using copy_constructor way
+
+            employees[1] = new Employee(employees[0]);
+
+            #endregion
+
+            #region third and fourth employee
+            employees[2] = new Employee(3, "ali", 26, 9500);
+            employees[3] = new Employee(4, "omar", 26, 6500);
+            #endregion
+
+            #region fifth employee a reference to the fourth employee (no copy constructor needed)
+
+            employees[4] = employees[3];
+
+            #endregion
+
+            #region sixth_employee
+            employees[5] = new Employee(5, "ayman", 22, 4300);
+            #endregion
+
+
+            #endregion
+
+
+            #region dispaly_employees
+            for (int i = 0; i <= employees.Length; i++)
+            {
+                Console.WriteLine($"Employee number {employees[i]}:");
+                employees[i].EmpInfo();
+                Console.WriteLine();
+            }
+            #endregion
+
+        }
+    }
+}
