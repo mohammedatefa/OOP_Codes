5442d43c47185b5cce936e494e3b925f7620b312
M.Atef <142452016+mohammedatefa@users.noreply.github.com>
2023-09-25 20:25:53 +0300

    OOP_Polymorphism
    apply the polymorphism in oop 
    1- constructor chaining using base 
    2- override using virtual, new ,abstract

--- Program.cs
+++ Program.cs
@@ -1,14 +1,76 @@
-﻿namespace Day_three
+﻿using System.Reflection.Metadata;
+
+namespace Day_fourandFive_oop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            manger manger1=new manger();
-            manger1.Adress="minya";
-            manger1.title = "manager";
-
-            line newline = new line("stringht", null,null);
+            Console.WriteLine("Hello, polymorphism!");
         }
     }
+    public class shape
+    {
+        public int x;
+        public int y;
+
+        public shape(int x,int y)
+        {
+            this.x=x;
+            this.y=y;  
+        }
+
+        public virtual void  display()
+        {
+            Console.WriteLine($"the x is{x} and y is {y}");
+        }
+    }
+    public class circle:shape
+    {
+        public int radiuse;
+        public circle(int radiuse) : base(1, 2)
+        {
+
+            this.radiuse = radiuse; 
+
+        }
+        public void area()
+        {
+            double area = (22 / 7) * Math.Sqrt(radiuse);
+            Console.WriteLine(area);
+        }
+        public void area(int radius)
+        {
+            double area = (22 / 7) * Math.Sqrt(radiuse);
+            Console.WriteLine(area);
+        }
+
+        public override void display()
+        {
+            base.x += 1;
+            base.y += 2;
+            Console.WriteLine($"the x is{x} and y is {y}");
+
+        }
+    }
+    public class rect : shape
+    {
+        public rect() : base(1, 2)
+        {
+
+        }
+    }
+
+    public class ellipse : circle
+    {
+        public ellipse() : base(10)
+        {
+
+        }
+        public new void display()
+        {
+            Console.WriteLine("we are in ellipse sahpe");
+        } 
+    }
+
 }
